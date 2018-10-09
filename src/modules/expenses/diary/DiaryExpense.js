6d1acdda2a67e8101ce6d97f81a59950a32c8adc
luis <luis@luiss-MacBook-Pro.local>
2018-10-09 13:42:18 -0500

    adding categories list

--- src/modules/expenses/diary/DiaryExpense.js
+++ src/modules/expenses/diary/DiaryExpense.js
@@ -4,15 +4,14 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { DiaryExpensesActions } from "../../../store/actions/expenses";
 import { Expense } from "../models/Expense";
-
+import styled from 'styled-components';
+import moment from 'moment';
 export class DiaryExpense extends React.Component {
   
   state = { ...new Expense() };
 
   save = () => {
-    this.props.save({...this.state }).then((response) => {
-      console.log('🔥 save:', response);
-    });
+    this.props.save({...this.state });
     this.setState({ ...(new Expense()) });
   };
 
@@ -20,10 +19,25 @@ export class DiaryExpense extends React.Component {
     this.setState({ [name]: e.target.value });
   };
 
+  onSelectCategoryList = (category) => {
+    this.setState({category})
+  }
+
+  updateDay = (days) => () => {
+    const day =  moment().add(days, 'days').format("YYYY-MM-DD");
+    this.setState({day});
+  }
+  
+
   render() {
     const isFormValid = this.state.category && this.state.amount && this.state.description
     return (
       <div className="flex flex-column flex-justify-content-center flex-align-items-center flex-grow-1 ">
+        <div className="margin-one width-90vw overflow-auto flex flex-justify-content-center">
+          <Styled.Button onClick={this.updateDay(-2)}>AnteAyer</Styled.Button>
+          <Styled.Button onClick={this.updateDay(-1)}>Ayer</Styled.Button>
+          <Styled.Button onClick={this.updateDay(0)}>hoy</Styled.Button>
+        </div>
         <div>
           <TextField
             id="date"
@@ -32,6 +46,9 @@ export class DiaryExpense extends React.Component {
             onChange={this.updateState('day')}
             value={this.state.day}
           />
+        </div>
+        <div className="margin-one width-90vw overflow-auto">
+          <CategoryList list={categories} selectItem={this.onSelectCategoryList} />
         </div>
         <div className="margin-one">
           <TextField
@@ -62,8 +79,7 @@ export class DiaryExpense extends React.Component {
               onClick={this.save} 
               variant="contained" 
               color="primary" 
-              disabled={!isFormValid}
-          >
+              disabled={!isFormValid}>
             Guardar
           </Button>
         </div>
@@ -72,8 +88,39 @@ export class DiaryExpense extends React.Component {
   }
 }
 
-const mapDispatchToProps = {
-  save: DiaryExpensesActions.save
-};
+const categories = [
+  'comida',
+  'transporte',
+  'bebe',
+  'casa',
+  'Mercado',
+  'ocio',
+]
 
-export default connect(null, mapDispatchToProps)(DiaryExpense);
+const CategoryList = ({ list , selectItem }) => {
+  return (<div className="flex">{ 
+    list.map(name => 
+      (<Categoryitem name={name} selectItem={selectItem}  />))}
+    </div>)
+}
+
+const Categoryitem = ({name, selectItem }) => {
+  const onClick = () => selectItem(name);
+  return(<Styled.Button onClick={onClick}>{name}</Styled.Button>)
+}
+
+
+
+export default connect(null, { save: DiaryExpensesActions.save })(DiaryExpense);
+
+
+const Styled = {
+  Button: styled.div`
+    background: aliceblue;
+    margin: .2rem;
+    padding: .5rem;
+    text-align: center;
+    border: 1px solid #ccc;
+    cursor: pointer;
+  `
+}
